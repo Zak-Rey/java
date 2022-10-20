f7e94e50912d901ce05f647833c545b3cd1f88ef
Zak <105656353+Zak-Rey@users.noreply.github.com>
2022-10-20 22:13:05 +0300

    Add files via upload

---
+++ js  function.js
@@ -0,0 +1,45 @@
+/*Function is a block of code that performs a specific task.
+Most programming paradigms have a huge number of inbuilt functions.
+
+SYNTAX FOR DECLARING A FUNCTION
+
+    function nameOfFunction () {
+        //body of the function
+    }
+
+The body of the function is written within curly braces.
+
+CALLING A FUNCTION.
+
+    nameOfFunction();
+
+Functions can also be declared with parameters(values passed during declaration of a function)
+
+EXAMPLE
+
+function greet(name) {
+    console.log("Hello " + name + ":)");
+}
+
+let name = promt("Enter a name: ");
+
+greet(name);
+
+In the above example, the greet function is declared with a name parameter.
+The user is prompted to enter a name. When the function is called. an argument is passed into the function.
+
+*NB -> When a value is passed when declaring a function, it is called a parameter.
+       when the function is called, the value passed is called an argument.
+
+hfhkj
+
+FUNCTION RETURN
+    The return statement can be used to return the value to a function call.
+    Any code after return statement is not executed, return denotes that the function has ended
+
+Benefits of using functions.
+    functions makes the code reusable.
+    functions make a programm easier as each task is divided into a function.
+    function increases readability.
+
+*/
