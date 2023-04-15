6283c5be36d5a95acafc1d3a31a21c43f0df6c4d
Zak-Rey <zaktripple@gmail.com>
2023-04-15 12:49:18 +0300

    Javascript typeconversion

--- js_introduction/js_typeconversion/js_implicitconversion/BooleanToNumber.js
+++ js_introduction/js_typeconversion/js_implicitconversion/BooleanToNumber.js
@@ -10,3 +10,5 @@ console.log(result); // 5
 
 result = 4 + false;
 console.log(result); // 4
+
+//Java script considers 0 as false and all non-zeros as true. It true is converted to a number it always result to 1
