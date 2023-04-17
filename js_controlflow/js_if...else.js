ce1c2e31583bcac9608391c42c86baa71ac21df9
Zak-Rey <zaktripple@gmail.com>
2023-04-17 21:53:25 +0300

    js control flow

--- js_controlflow/js_if...else.js
+++ js_controlflow/js_if...else.js
@@ -1,7 +1,7 @@
 const number = prompt("Enter a number: ");
 
 if (number > 0){
-    console.log("The number is positive");
+    console.log("The number is positive.");
 } else {
     console.log("the number is negative.")
 }
