923de29110362de2ccc6e9fc603c38772e4f5b31
Zak-Rey <zaktripple@gmail.com>
2023-04-24 21:03:52 +0300

    java script switch statement

--- js_controlflow/js_switch_sstatement.js
+++ js_controlflow/js_switch_sstatement.js
@@ -1,1 +1,15 @@
 //used in decision making, it evaluates an expression and excecutes the corresponding body that matches the expression's result.
+let a = 2;
+
+switch (a) {
+    case 1:
+        a = 'one';
+        break;
+    case 2:
+        a = 'two';
+        break;
+    default:
+        a = 'not found';
+        break;
+}
+console.log(`The value is ${a}`);
